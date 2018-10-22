f71a62a9e2ed03dd7038d7bc8025589bd287fff0
Alla Yevtukh <a.yevtukh@gmail.com>
2018-10-22 09:25:45 +0300

    Nice home page.

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -3,7 +3,7 @@ import * as React from 'react';
 export class HomePage extends React.Component {
     public render() {
         return (
-            <div>Home Page</div>
+            <div>Nice Home Page</div>
         )
     }
 }
