19e9126223f0c2a9dde823a40fc5db60ac8e475b
Alla Yevtukh <a.yevtukh@gmail.com>
2018-10-22 09:50:17 +0300

    Add hello message.

--- src/pages/HomePage.tsx
+++
@@ -1,9 +0,0 @@
-import * as React from 'react';
-
-export class HomePage extends React.Component {
-    public render() {
-        return (
-            <div>Nice Home Page</div>
-        )
-    }
-}
