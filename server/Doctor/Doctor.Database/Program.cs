c9450d8732045530dfdd51b2b299fb41164e74c4
Alla Yevtukh <a.yevtukh@gmail.com>
2018-10-23 10:15:29 +0300

    Add database project.

---
+++ server/Doctor/Doctor.Database/Program.cs
@@ -0,0 +1,43 @@
+﻿using DbUp;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tasks.Server.Database
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var connectionString = args.FirstOrDefault() ?? ConfigurationManager.ConnectionStrings["doctor"].ConnectionString;
+
+            var upgrader = DeployChanges.To
+                    .SqlDatabase(connectionString)
+                    .WithScriptsEmbeddedInAssembly(Assembly.GetExecutingAssembly())
+                    .LogToConsole()
+                    .Build();
+
+            var result = upgrader.PerformUpgrade();
+
+            if (!result.Successful)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(result.Error);
+                Console.ResetColor();
+#if DEBUG
+                Console.ReadLine();
+#endif
+                return;
+            }
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Success!");
+            Console.ResetColor();
+            return;
+        }
+    }
+}
